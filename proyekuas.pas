830c5f4544f9d50612b4ba24484bfe1673960025
RizkiNandas <adianpsb1979@gmail.com>
2024-12-09 16:36:08 +0700

    update awal

--- proyekuas.pas
+++ proyekuas.pas
@@ -1,1 +1,13 @@
+program menjagaberatbadanideal;
 uses crt;
+type 
+    orang = record
+    nama:string;
+    tinggi:real;
+    tinggibmi:real;
+    berat:real;
+    bmi:real;
+    kategori:string;
+    jeniskelamin:string;
+    ideal:real;
+    end;
