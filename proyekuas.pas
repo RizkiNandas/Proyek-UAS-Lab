abec3d8cc611f7431d018fbe133a10dd0fc64359
jonthanngl <jonathannenggolan@gmail.com>
2024-12-09 16:40:55 +0700

    Update proyekuas.pas

--- proyekuas.pas
+++ proyekuas.pas
@@ -15,3 +15,15 @@ var
     banyakorang:array [1..10] of orang;
     jumlahorang,i:integer;
     lanjut:char;
+procedure hitungideal (var data:orang);
+    begin
+        with data do
+        begin
+            if (jeniskelamin = 'Pria') or (jeniskelamin = 'pria') then  
+                ideal := (tinggi - 100) - (10/100 * (tinggi - 100))
+            else if (jeniskelamin = 'Wanita') or (jeniskelamin = 'wanita') then
+                ideal := (tinggi - 100) - (15/100 * (tinggi - 100))
+            else
+                writeln('Jenis Kelamin Tidak Valid');
+        end;
+    end;
